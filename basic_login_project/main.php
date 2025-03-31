1328695325ba0e925e073328ed971bed8ba6d810
tjjames230 <108950500+tjjames230@users.noreply.github.com>
2025-03-31 15:40:17 -0500

    update: adding some very basic login functionality

---
+++ basic_login_project/main.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Basic Login Project</title>
+</head>
+
+<body>
+	<h1>Welcome</h1>
+	<p>Please login to continue:</p>
+	<p>("user" for username and "pass" for password.)</p>
+	<form action="login.php" method="POST">
+		<label for="username">Username: </label>
+		<input type="text" name="username" id="username">
+		<label for="password">Password: </label>
+		<input type="text" name="password" id="password">
+		<button type="submit">Submit</button>
+	</form>
+
+
+	<?php
+
+	session_start();
+
+
+
+	?>
+
+
+</body>
+
+</html>
